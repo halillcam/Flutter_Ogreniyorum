626561488e3bdaa3a1b32cbdd92644f51a70f387
Halil Ibrahim Cam <halil.cam0020@gmail.com>
2024-12-05 23:57:01 +0300

    Text ve button eklendi, button u kıvrımlı yaptık

--- flutter_ogreniyorum/lib/anasayfa.dart
+++ flutter_ogreniyorum/lib/anasayfa.dart
@@ -22,12 +22,16 @@ class _AnasayfaState extends State<Anasayfa> {
           Padding(padding: 
           const EdgeInsets.all(8),
           child: Text("Beef Cheese",style: TextStyle(fontSize: 36,color: Anarenk,fontWeight: FontWeight.bold),),
+          
           ),
+           Container(width: 250,height: 180,
+           child: Image.asset("images/pizza.png"),),
+           
           //Image.asset("images/pizza.png"),
 
           Padding(padding: const EdgeInsets.only(top: 16),
            child: Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           
+  
             children: [
               TextButton(onPressed: (){},
                style: TextButton.styleFrom(backgroundColor: Anarenk),
@@ -50,9 +54,32 @@ class _AnasayfaState extends State<Anasayfa> {
                ),
             ],
           )
-          )
+          ),
 
-         
+          Padding(padding: const EdgeInsets.all(16),
+          child: Column(
+              children: [
+                Text("20 min",style: TextStyle(color: yaziRenk2,fontSize: 22,fontWeight: FontWeight.bold),),
+                Text("Delivery",style: TextStyle(color: Anarenk,fontSize: 22,fontWeight: FontWeight.bold),),
+                Text("Pizza Lover",style: TextStyle(color: yaziRenk2,fontSize: 22,),),
+              ],
+          ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: [
+              Text("\$ 5.98",style: TextStyle(fontSize: 44,color: Anarenk,fontWeight: FontWeight.bold),),
+              //const Spacer(), bununla ikisi arasında ki mesafeyi uzatırız yani birini sağ koseye yapıstırırız digerınıde sol koseye
+              TextButton(onPressed: (){},
+               style: TextButton.styleFrom(backgroundColor: Anarenk,
+               shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5)))
+               ),
+               child: Text("ADD TO CART",style: TextStyle(color: yaziRenk1,fontSize: 18,),),
+               ),
+            ],
+          )
         ],
       ),
       );
